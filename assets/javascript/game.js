95feda98a70edb1fcdf819279905085cc12d3a8c
Logan McNulty <logan.mcnulty@zmfs.local>
2019-08-26 17:36:01 -0400

    back to using old javascript because new method did not work out

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,15 +14,16 @@
 
 //function that begins the game and used to reset the game
     function gameStart(){
-    secretWord = stockWords[Math.floor(Math.random() * stockWords.length)];
-    console.log(secretWord); // peak at the log if you want to cheat!
+    //assign secret word randomly
+        secretWord = stockWords[Math.floor(Math.random() * stockWords.length)];
+        console.log(secretWord); // peak at the log if you want to cheat!
 
-    secretWordText = []; //empties the array where correct answers are stored
+        secretWordArray = []; //empties the array where correct answers from previous game are stored
 
     //function that adds the correct number of underscores to the secret word text
-    for(var i = 0; i <secretWord.length; i++) {
-        secretWordArray.push("_");
-    } //push as many underscores to the secretWordArray as there are letters in the secret word
+        for(var i = 0; i <secretWord.length; i++) {
+            secretWordArray.push("_");
+        } //push as many underscores to the secretWordArray as there are letters in the secret word
 
     var secretWordText = document.getElementById("secretWordInsert"); //update the HTML ID secretWordInsert with 
     secretWordText.textContent = secretWordArray.join(" "); //define secretWordText contenet, and add a space between the underscores
@@ -34,6 +35,85 @@
     //reset hangman pic
     document.getElementById("hungManDiv").src="assets/images/hung.png";
 
-    }
+    pushToHTML();
+    };
 
-    gameStart();
+//function that pushes variable/text to the HTML
+    function pushToHTML() {
+        document.getElementById("wins").innerText = wins;
+        document.getElementById("losses").innerText = losses;
+        document.getElementById("guessesRemaining").innerText = guessesRemaining;
+        document.getElementById("guessesSoFar").innerText = guessesSoFar;
+        document.getElementById("secretWordInsert").innerText = secretWordArray.join(" ");
+    };
+
+//function to compare the guessed letters to the secret word
+    function checkForMatch(guess) {
+    //if guessesSoFar does not include the guess (this prevents allowing the user to guess the same letter twice)...
+        if (guessesSoFar.indexOf(guess) === -1) {
+        //then push the guess into the guessesSoFar array
+            guessesSoFar.push(guess);
+            //if guess is not in secret word, deduct guesses remaining
+                if (secretWord.indexOf(guess) === -1) {
+                    guessesRemaining--;
+                    //update hang man pic according to guesses remaining
+                    if(guessesRemaining === 7) {document.getElementById("hungManDiv").src="assets/images/hung1.png";};
+                    if(guessesRemaining === 6) {document.getElementById("hungManDiv").src="assets/images/hung2.png";};
+                    if(guessesRemaining === 5) {document.getElementById("hungManDiv").src="assets/images/hung3.png";};
+                    if(guessesRemaining === 4) {document.getElementById("hungManDiv").src="assets/images/hung4.png";};
+                    if(guessesRemaining === 3) {document.getElementById("hungManDiv").src="assets/images/hung5.png";};
+                    if(guessesRemaining === 2) {document.getElementById("hungManDiv").src="assets/images/hung6.png";};
+                    if(guessesRemaining === 1) {document.getElementById("hungManDiv").src="assets/images/hung7.png";};
+                    if(guessesRemaining === 1) {document.getElementById("hungManDiv").src="assets/images/hung8.png";};
+            }
+            //otherwise, the guessed letter is in the secretWord, and update the secret word array
+            else{
+                //loop through the secret word as many times as the word is long
+                for (var i=0; i < secretWord.length; i++) {
+                    //if guess = a position of the secretword, update that same index(s) in the secret word array
+                    if (guess === secretWord[i]) {
+                        secretWordArray[i] = guess;
+                    }
+                }
+            }
+        }
+};
+
+function isWinner() {
+    //if secret word array does not include underscores then you win and reset the game
+    if(secretWordArray.indexOf("_") === -1) {
+        wins++;
+        resetGame=true;
+    }
+};
+
+function isLoser () {
+    //if guesses remaining reaches 0 then add to losses and reset the game
+    if(guessesRemaining <= 0) {
+        losses++;
+        resetGame=true;
+    }
+};
+
+//log key strokes from user
+document.onkeyup = function (x) {
+    var userGuess = x.key;
+    //if resetGame = true due to isWinner or isLoser, then launch gameStart() function to reset. Then switch resetGame = false in order to play 
+    if(resetGame) {
+        gameStart();
+        resetGame=false;
+        checkForMatch(userGuess);
+        pushToHTML();
+        isWinner();
+        isLoser();
+    }
+    console.log(userGuess);
+    console.log(guessesRemaining);
+    console.log(guessesRemainingText);
+    console.log(guessesSoFar);
+};
+
+gameStart();
+pushToHTML();
+
+    
---
+++ assets/javascript/game_old.js
@@ -0,0 +1,79 @@
+// create an array, and random word is chosen from array
+const stockWords = ["bull", "bear", "dividend", "earnings", "sector", "growth", "nasdaq", "broker", "options", "equity", "trend", "price"]
+var secretWord = stockWords[Math.floor(Math.random() * stockWords.length)];
+console.log(secretWord);
+
+// create underscores based on length of the word
+var underScores = [];
+var underscoreGenerator = () => {
+    for(var i = 0; i <secretWord.length; i++) {
+        underScores.push("_");
+    }
+    return underScores;
+}
+
+//push  array to #secretWordInsert and generate underscores by calling underscoreGenerator function
+var secretWordText = document.getElementById("secretWordInsert");
+underscoreGenerator();
+secretWordText.textContent = underScores.join(" ");
+
+//game score variables and accompanying variables for updating the HTML
+var wins = 0;
+var userWinsText = document.getElementById("wins");
+var losses = 0;
+var userLossesText = document.getElementById("losses");
+var guessesRemaining = 8;
+var guessesRemainingText = document.getElementById("guessesRemaining");
+var guessesSoFarText = document.getElementById("guessesSoFar");
+
+
+// create a correct/incorrect guesseses arrays to have the user's guesses pushed accordingly
+var correctGuesses = [];
+var incorrectGuesses = [];
+
+//start tracking user key strokes
+document.onkeyup = function (event) {
+//store user choice to key selected
+    var userGuess = event.key;
+//if user guess matches a letter of the secret word...
+    if(secretWord.includes(userGuess)){
+// then push that letter to the correctGuesses array
+    correctGuesses.push(userGuess);
+// using the user guess, update the index of the underScores array that matches the index of the Secret word that the user guessed
+    underScores[secretWord.indexOf(userGuess)] = userGuess;
+// update the secret word text content, and join the secret word text using a space instead of commas that is default for array values
+    secretWordText.textContent = underScores.join(" ");
+    console.log(secretWordText);
+}
+//else, add the incorrect userGuess to the incorrectGuesses array, update the GuessesSoFar text content in the HTML, and 
+// deduct one point from GuessesRemaining
+    else{
+        incorrectGuesses.push(" " + userGuess);
+        console.log("incorrect guesses: " + incorrectGuesses);
+        guessesSoFarText.textContent = incorrectGuesses;
+        guessesRemaining--;
+        console.log(guessesRemaining);
+        guessesRemainingText.textContent = guessesRemaining;
+    }
+
+// code for updating the hangman image (probably redundant)
+    if(guessesRemaining === 7) {document.getElementById("hungManDiv").src="assets/images/hung1.png";};
+    if(guessesRemaining === 6) {document.getElementById("hungManDiv").src="assets/images/hung2.png";};
+    if(guessesRemaining === 5) {document.getElementById("hungManDiv").src="assets/images/hung3.png";};
+    if(guessesRemaining === 4) {document.getElementById("hungManDiv").src="assets/images/hung4.png";};
+    if(guessesRemaining === 3) {document.getElementById("hungManDiv").src="assets/images/hung5.png";};
+    if(guessesRemaining === 2) {document.getElementById("hungManDiv").src="assets/images/hung6.png";};
+    if(guessesRemaining === 1) {document.getElementById("hungManDiv").src="assets/images/hung7.png";};
+    if(guessesRemaining === 0) {
+        document.getElementById("hungManDiv").src="assets/images/hung8.png";
+        losses++;
+        console.log(losses);
+        userLossesText.textContent = losses;
+    };
+
+    if(secretWordText === secretWord){
+        console.log("you win");
+    }
+
+
+};
